28cd7898792165a71235a29ca7bd440beea5ea51
JulesRuby <JulesRuby@users.noreply.github.com>
2020-12-14 21:13:32 -0700

    implemented basic menu open/close for mobile

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // js stuffffff
+const hamburger = document.querySelector('.menu-button');
+const menu = document.querySelector('.menu-list');
+
+hamburger.addEventListener('click', () => {
+	menu.classList.toggle('open');
+	hamburger.classList.toggle('open');
+})
