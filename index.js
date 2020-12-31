40ad6c16212418479b8560608a9e0a9a457df3b2
JulesRuby <JulesRuby@users.noreply.github.com>
2020-12-31 11:21:17 -0700

    fixed sizing and layout for some section, applied blur filters

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // js stuffffff
 const hamburger = document.querySelector('.menu-button');
 const menu = document.querySelector('.menu-list');
+const socialMenu = document.querySelector('.social-list');
 
 hamburger.addEventListener('click', () => {
 	menu.classList.toggle('open');
+	socialMenu.classList.toggle('open');
 	hamburger.classList.toggle('open');
 })
