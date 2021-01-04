46c4666589641dd62e44327111d984f6f4f2f350
JulesRuby <JulesRuby@users.noreply.github.com>
2021-01-04 01:55:19 -0700

    refactored js some more

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 // Store query selectors to shorten code, basically ape jQuery a bit
 const $ = document.querySelector.bind(document);
+
+// I can probably use this ($$) to assign the dom nodes currently stored individually below, instead to a singular array and then just run the forEach on that named array, rather than using the destructuring [x, y, z] = [$('.x'), $('.y'), $('.z')] method
+// However, I'm  going to leave this as is for now, though. In case I find that I need more control over the individual nodes.
 const $$ = document.querySelectorAll.bind(document);
 
 // topNav for making mobile nav hide on scroll
-const topNav = $('.navigation');
+// assign this on it's own if I decide to use the $$ to create an array for all the elements affected by the .open class
+// const topNav = $('.navigation');
 
 // Elements for opening and closing mobile menu
 // Making use of destructuring to assign nodes to variables
-const [hamburger, menu, socialMenu, body] = [
+const [topNav, hamburger, menu, socialMenu, body] = [
+	$('.navigation'),
 	$('.menu-button'),
 	$('.menu-list'),
 	$('.social-list'),
@@ -21,44 +26,26 @@ hamburger.addEventListener('click', () => {
 	);
 });
 
-// Store vertical scroll position
-let previousScrollPosition = window.pageYOffset;
-// Store viewport width to conditionally apply scrollHide only on mobile
-let previousViewportWidth = window.innerWidth;
 // Create a boolean that trips when scrollHide is no longer needed
 // Evaluated on pageload, and then again later when resizing occurs
 const scrollToggle = () => (window.innerWidth <= 768 ? true : false);
 scrollToggle();
-// console.log(scrollToggle())
 
-// Create a function to hide the mobile nav on page scroll
-// const scrollHide = () => {
-// 	window.addEventListener('scroll', () => {
-// 		// Store current vertical position to measure against the previous
-// 		const currentScrollPosition = window.pageYOffset;
-// 		const scrollDistance = currentScrollPosition - previousScrollPosition;
+// This formatting is cancer, fix linting config later********************
+const toggleListener = () => {
+	if (scrollToggle() === true) {
+		window.addEventListener('scroll', scrollHide);
+	} else {
+		window.removeEventListener('scroll', scrollHide);
+		topNav.classList.remove('scroll-hide');
+		[hamburger, menu, socialMenu, body].forEach((el) =>
+			el.classList.remove('open')
+		);
+	}
+};
 
-// 		// If the user has not scrolled beyond X pixels of distance, stop here
-// 		if (
-// 			currentScrollPosition < 0 ||
-// 			(scrollDistance < 200 && scrollDistance > -200)
-// 		) {
-// 			return;
-// 		}
-
-// 		// If user has scrolled down enough, hide nav
-// 		if (scrollDistance >= 200) {
-// 			topNav.classList.add('scroll-hide');
-// 			previousScrollPosition = currentScrollPosition;
-// 		}
-
-// 		// If user has scrolled up enough, reveal nav
-// 		if (parseFloat(scrollDistance) <= -200) {
-// 			topNav.classList.remove('scroll-hide');
-// 			previousScrollPosition = currentScrollPosition;
-// 		}
-// 	});
-// };
+// Store vertical scroll position for use in scrollHide fn
+let previousScrollPosition = window.pageYOffset;
 
 const scrollHide = () => {
 	// Store current vertical position to measure against the previous
@@ -86,44 +73,6 @@ const scrollHide = () => {
 	}
 };
 
-// if (scrollSwitch === true) window.addEventListener('scroll', scrollHide);
-
-const toggleListener = () => {
-	if (scrollToggle() === true) {
-		window.addEventListener('scroll', scrollHide);
-	} else {
-		window.removeEventListener('scroll', scrollHide);
-		topNav.classList.remove('scroll-hide');
-	}
-};
-
 toggleListener();
 
-// window.addEventListener('resize', () => {
-// 	const currentViewportWidth = window.innerWidth;
-
-// 	if (
-// 		(previousViewportWidth <= 768 && currentViewportWidth <= 768) ||
-// 		(previousViewportWidth > 768 && currentViewportWidth > 768)
-// 	) {
-// 		return;
-// 	} else if (previousViewportWidth <= 768 && currentViewportWidth > 768) {
-// 		window.removeEventListener('scroll', scrollHide);
-// 		previousViewportWidth = currentViewportWidth;
-// 	} else if (previousViewportWidth > 768 && currentViewportWidth <= 768) {
-// 		window.addEventListener('scroll', scrollHide);
-// 		previousViewportWidth = currentViewportWidth;
-// 	}
-// });
-
 window.addEventListener('resize', toggleListener);
-
-// if previous width and current width are either:
-// both <= 768 or both > 768
-// nothing is changed
-
-// but if the previous is either <= or >  than 768
-// and current is meets the opposite criteria
-// then we trigger the addition/removal of scrollHide event listener
-
-// console.log(viewportWidth);
